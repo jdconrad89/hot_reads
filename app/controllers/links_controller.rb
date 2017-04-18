729220ab6bd6cf6b9692449595c62061ac105cfa
Jason Conrad <jason.d.conrad.89@gmail.com>
2017-04-18 15:33:03 -0600

    refactor the links controller to push logic down the stack

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -2,10 +2,7 @@ class LinksController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
-    @links = Link.all.order(read_count: :DESC).limit(10)
+    @links = Link.hot_reads
   end
-
-def create
-end
 
 end
